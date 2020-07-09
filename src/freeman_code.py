9064abdc754da43a885ccd1490116cf0b18f5042
zhang qi <qcymkxyc@163.com>
2020-07-09 16:26:28 +0800

    添加第10章的数据

--- src/freeman_code.py
+++ src/freeman_code.py
@@ -10,10 +10,15 @@
 @Description    :
 """
 import numpy as np
+from deprecated.sphinx import deprecated
 
 """表示是按4方向编码还是按8方向编码"""
 FOUR_DIRECTION = 1
 EIGHT_DIRECTION = 2
+
+# =========================================================
+# 获取Freeman链码坐标
+# =========================================================
 
 
 def __get_freeman_box_list(
@@ -44,6 +49,24 @@ def __get_freeman_box_list(
 
     return freeman_x_coordination_list, freeman_y_coordination_list
 
+# ===============================================================
+# 获取边缘对应的Freeman链码坐标
+# ===============================================================
+
+
+def __get_close_num(num: float, num_list: [float]) -> (float, float):
+    """给定一个数字以及一个有序的list，返回离该数字最近的两个数组中的元素
+
+    :param num: float,该数字
+    :param num_list: List[float],有序的list
+    :return: Tuple(flaot,float),最近的两个元素
+    """
+    for i in range(len(num_list) - 1):
+        current_num = num_list[i]
+        next_num = num_list[i + 1]
+        if current_num <= num <= next_num:
+            return current_num, next_num
+
 
 def __get_freeman_coordination(
         point: (int, int), freeman_x_list: list, freeman_y_list: list) -> (int, int):
@@ -52,31 +75,125 @@ def __get_freeman_coordination(
     :param point: (int,int), 点的坐标
     :param freeman_x_list: List[float], Freeman网格x
     :param freeman_y_list: List[float], Freeman网格y
-    :return:
+    :return: (int,int) or None
+        如果该点在Freeman网格线上则返回对应的Freeman网格坐标，否则返回None
     """
     x, y = point
-    '''如果该点在网格的线上'''
+    x_index, y_index = None, None
+    """如果该点在网格的线上"""
     if x in freeman_x_list or y in freeman_y_list:
         try:
             x_index = freeman_x_list.index(x)
         except ValueError:
             '''如果该点在横线上'''
-            # TODO
-            pass
+            last_freeman_x, next_freeman_x = __get_close_num(x, freeman_x_list)
+            mean_freeman_x = (last_freeman_x + next_freeman_x) / 2
+            x_index = freeman_x_list.index(last_freeman_x)
+            x_index = x_index if x < mean_freeman_x else x_index + 1
+            y_index = freeman_y_list.index(y)
+
+            return x_index, y_index
+
         else:
+            """如果该点在纵线上"""
             try:
                 y_index = freeman_y_list.index(y)
             except ValueError:
                 '''如果该点在纵线上'''
-                # TODO
-                pass
+                last_freeman_y, next_freeman_y = __get_close_num(
+                    y, freeman_y_list)
+                mean_freeman_y = (last_freeman_y + next_freeman_y) / 2
+                y_index = freeman_y_list.index(last_freeman_y)
+                y_index = y_index if y < mean_freeman_y else y_index + 1
+                x_index = freeman_x_list.index(x)
             else:
                 '''如果该点刚好在Freeman编码的坐标点上'''
-                # TODO
-                pass
+                x_index = freeman_x_list.index(x)
+                y_index = freeman_y_list.index(y)
+        finally:
+            return x_index, y_index
     else:
         '''如果没有'''
         return
+
+
+# ============================================================
+# FreeMan编码
+# ============================================================
+@deprecated(version = "1.0",reason="该函数判断不合理")
+def __is_freeman_coordination_correct(freeman_coordination_list:[(int,int)]) -> bool:
+    """验证Freeman链码是否正确
+    
+    如果下一个点和当前点仅差一个距离（即8个方向），则判定下一个点是正确的，否则判定下一个
+    点是错误的
+    
+    :param freeman_coordination_list: List[(int,int)],得到的Freeman链码的List
+    :return: bool,链码是否正确
+    """
+    for i in range(len(freeman_coordination_list) - 1):
+        current_freeman_point = freeman_coordination_list[i]
+        current_point_x,current_point_y = current_freeman_point
+        next_freeman_point = freeman_coordination_list[i + 1]
+
+        # 8个方向候选点(包括当前点),如果在候选点中得到超出范围的Freeman坐标点，不影响结果
+        candidate_point_list = list()
+        for x_direct_offset in [-1,0,1]:
+            for y_direct_offset in [-1,0,1]:
+                candidate_point_list.append((current_point_x + x_direct_offset,current_point_y + y_direct_offset))
+        if next_freeman_point not in candidate_point_list:
+            print(current_freeman_point,next_freeman_point)
+            return False
+    return True
+
+
+def __get_freeman_code_by_two_point(point1:(int,int),point2:(int,int),) -> int:
+    """给定两个点（Freeman点），返回Freeman编码
+
+    :param point1: (int,int),点1
+    :param point2: (int,int),点2
+    :return: int， Freeman编码
+    """
+    freeman_code_dict = {
+        str((1,0)): 0,
+        str((1,1)): 1,
+        str((0,1)): 2,
+        str((-1,1)): 3,
+        str((-1,0)): 4,
+        str((-1,-1)): 5,
+        str((0,-1)) : 6,
+        str((1,-1)) : 7,
+    }
+    point_gap = (point2[0] - point1[0],point2[1] - point1[1])
+    try:
+        print(str(point_gap))
+        return freeman_code_dict[str(point_gap)]
+    except KeyError:
+        return
+
+
+def __get_freeman_code(freeman_coordination_list:[(int,int)]) -> [int]:
+    """给定Freeman坐标，返回Freeman编码的结果
+
+    :param freeman_coordination_list: List[(int,int)],Freman坐标组成的List
+    :return: List[int],Freeman编码结果
+    """
+    freeman_code_list = list()
+    for i in range(len(freeman_coordination_list) - 1):
+        current_freeman_point = freeman_coordination_list[i]
+        next_freeman_point = freeman_coordination_list[i + 1]
+
+        freeman_code = __get_freeman_code_by_two_point(
+            point1=current_freeman_point,
+            point2=next_freeman_point)
+        '''排除重复点 '''
+        if freeman_code is not None:
+            freeman_code_list.append(freeman_code)
+    return freeman_code_list
+
+
+# ============================================================
+# 主函数
+# ============================================================
 
 
 def get_freeman_code(img: np.ndarray, contour: [
@@ -88,12 +205,30 @@ def get_freeman_code(img: np.ndarray, contour: [
     :param mode: int,编码方式，表示是按4方向编码还是按8方向编码
     :return: [int], 弗雷曼链码
     """
+    freeman_coordination_list = list()  # 用于保存得出的Freeman链码坐标
     '''获取Freeman链码的网格坐标'''
     freeman_x_coordination_list, freeman_y_coordination_list = \
         __get_freeman_box_list(img=img)
 
+    """得到边缘在Freeman链码上的坐标"""
     for point in contour:
         x, y = point
-        if x in freeman_x_coordination_list and y in freeman_y_coordination_list:
-            # TODO
-            pass
+        point_freeman_coordination = __get_freeman_coordination(
+            point=(x, y),
+            freeman_x_list=freeman_x_coordination_list,
+            freeman_y_list=freeman_y_coordination_list,
+        )
+        """如果该点在Freeman网格上"""
+        if point_freeman_coordination is not None:
+            freeman_coordination_list.append(point_freeman_coordination)
+
+    '''添加最后一个点到第一个点的连线'''
+    freeman_coordination_list.append(freeman_coordination_list[0])
+
+    # return freeman_coordination_list
+    """验证得到的Freeman坐标是否正确"""
+    assert __is_freeman_coordination_correct(freeman_coordination_list)
+
+    """根据方向得到Freeman链码"""
+    return __get_freeman_code(freeman_coordination_list)
+
--- tests/test_freeman_code.py
+++ tests/test_freeman_code.py
@@ -10,7 +10,9 @@
 @Description    :
 """
 from unittest import TestCase
+import cv2
 import unittest
+from PIL import Image
 from src import freeman_code
 from PIL import Image
 import os
@@ -19,6 +21,11 @@ import numpy as np
 
 def get_freeman_box_list(img, n_box_x, n_box_y):
     return freeman_code.__get_freeman_box_list(img, n_box_x, n_box_y)
+
+
+def get_freeman_coordination(point, freeman_x_list, freeman_y_list):
+    return freeman_code.__get_freeman_coordination(
+        point, freeman_x_list, freeman_y_list)
 
 
 class FreemanTest(TestCase):
@@ -31,18 +38,62 @@ class FreemanTest(TestCase):
         self.img = np.asarray(self.img)
 
     def test_get_freeman_code(self):
-        pass
+        import matplotlib.pyplot as plt
+        img_path = "../image_data/DIP3E_CH11_Original_Images/Fig1105(a)(noisy_stroke).tif"
+        img = Image.open(img_path)
+        img = np.asarray(img)
+
+        tmp_img = cv2.blur(img,(9,9))
+        th,tmp_img = cv2.threshold(tmp_img,0,255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        image,contours,hierarchy = cv2.findContours(tmp_img,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+        contour = contours[-1]
+        contour = np.reshape(contour,(contour.shape[0],contour.shape[-1]))
+        x = [i[0] for i in contour]
+        y = [i[1] for i in contour]
+        plt.plot(x,y)
+        plt.show()
+
+        a = freeman_code.get_freeman_code(tmp_img,contour)
+        print(a)
+        # x = [i[0] for i in a]
+        # y = [i[1] for i in a]
+        # plt.plot(x,y)
+        # plt.show()
+
+    def test_get_freeman_coordination(self):
+        freeman_x_list = np.arange(0, 100, 10).tolist()
+        freeman_y_list = np.arange(0, 100, 10).tolist()
+
+        """刚好在交点上"""
+        point = (10, 10)
+        r = get_freeman_coordination(point, freeman_x_list, freeman_y_list)
+        self.assertEqual(r, (1, 1))
+
+        """在横轴上"""
+        point = (10, 22)
+        r = get_freeman_coordination(point, freeman_x_list, freeman_y_list)
+        self.assertEqual(r, (1, 2))
+
+        """在纵轴上"""
+        point = (22, 10)
+        r = get_freeman_coordination(point, freeman_x_list, freeman_y_list)
+        self.assertEqual(r, (2, 1))
+
+        """没在线上"""
+        point = (22, 22)
+        r = get_freeman_coordination(point, freeman_x_list, freeman_y_list)
+        self.assertIsNone(r)
 
     def test_get_freeman_box_list(self):
         n_box_x = 10
         n_box_y = 10
-        x_list,y_list = get_freeman_box_list(self.img,n_box_x,n_box_y)
-        self.assertEqual(len(x_list),n_box_x + 1)
-        self.assertEqual(len(y_list),n_box_y + 1)
+        x_list, y_list = get_freeman_box_list(self.img, n_box_x, n_box_y)
+        self.assertEqual(len(x_list), n_box_x + 1)
+        self.assertEqual(len(y_list), n_box_y + 1)
 
-        img_h,img_w = self.img.shape
-        self.assertLessEqual(x_list[-1],img_w)
-        self.assertLessEqual(x_list[-1],img_h)
+        img_h, img_w = self.img.shape
+        self.assertLessEqual(x_list[-1], img_w)
+        self.assertLessEqual(x_list[-1], img_h)
 
 
 if __name__ == "__main__":
